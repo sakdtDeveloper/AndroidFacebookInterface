9f130f88c700608a1ba41b0685a8de9835a87594
sakdt <sakdt5@web.de>
2021-11-12 14:01:48 +0100

    simple listview with adapter

--- app/src/main/java/de/thm/ap/androidfacebookinterface/HomeActivity.kt
+++ app/src/main/java/de/thm/ap/androidfacebookinterface/HomeActivity.kt
@@ -2,6 +2,8 @@ package de.thm.ap.androidfacebookinterface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
@@ -9,6 +11,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+
+
+      val listPost:ListView=findViewById(R.id.listPosts)
+       val postList= listOf("Post 1","Post 2","Post 3","Post 4","Post 5")
+        listPost.adapter=ArrayAdapter(this,android.R.layout.simple_list_item_1,postList)
 
     }
 }
