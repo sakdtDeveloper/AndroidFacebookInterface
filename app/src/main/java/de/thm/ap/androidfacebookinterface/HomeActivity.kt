dca41eb48ad1015d77cf95b2af793d3643275ffc
sakdt <sakdt5@web.de>
2021-11-13 18:59:16 +0100

    Menu :ContextMenu function afficher et delete

--- app/src/main/java/de/thm/ap/androidfacebookinterface/HomeActivity.kt
+++ app/src/main/java/de/thm/ap/androidfacebookinterface/HomeActivity.kt
@@ -3,14 +3,17 @@ package de.thm.ap.androidfacebookinterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var listPost:ListView
+     var postList=ArrayList<Post>()
+    lateinit var adapter:PostAdapter
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +22,16 @@ class HomeActivity : AppCompatActivity() {
 
 
 
-      val listPost:ListView=findViewById(R.id.listPosts)
-       val postList= arrayListOf(
+       listPost=findViewById(R.id.listPosts)
+        postList= arrayListOf(
            Post("sommer","couche de soleil, plage , ibiza",R.drawable.image1),
            Post("ocean"," blue, filipin",R.drawable.image2),
            Post("Post3","couche de soleil, plage , ibiza",R.drawable.image3),
            Post("Post4","sonido",R.drawable.image4),
            Post("Post5","amaterazu",R.drawable.image5))
 
-        listPost.adapter=PostAdapter(this,R.layout.item_post,postList)
+        adapter=PostAdapter(this,R.layout.item_post,postList)
+        listPost.adapter=adapter
 
         listPost.setOnItemClickListener { adapterView, view, position, id ->
             Intent(this,PostDetailActivity::class.java).apply {
@@ -40,7 +44,9 @@ class HomeActivity : AppCompatActivity() {
             }
         }
 
+       //initialisation du contextMenu sur la liste
 
+        registerForContextMenu(listPost)
 
     }
     //ajouter le Optionmenu
@@ -69,4 +75,41 @@ class HomeActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    //ajouter un contextMenu
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.list_context_menu, menu)
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+    //ajouter un ecouteur au contextMenu
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+
+        //recuperer la postion de du item dans la list grace au parametre menuInfo dans oncreateContextMenu
+        val position=(item.menuInfo as AdapterView.AdapterContextMenuInfo).position
+
+        when(item.itemId){
+            R.id.item_show->{
+                Intent(this,PostDetailActivity::class.java).apply {
+
+                    putExtra("title",postList[position].title)
+                    putExtra("description",postList[position].description)
+                    putExtra("image",postList[position].image)
+
+                    startActivity(this)
+                }
+            }
+            R.id.item_delete->{
+              postList.removeAt(position)
+                //signaler la suppression a l'adapteur
+                adapter.notifyDataSetChanged()
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
 }
